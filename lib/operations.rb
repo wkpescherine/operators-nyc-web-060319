c17b5c1370c5d1b88680c2f4658f8e7739eaaf9d
William Pescherine <wkpescherine@gmail.com>
2019-05-21 04:52:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-  if speed > 60
-    true
-  elsif speed >40 && speed <60
-    false
-  else 
-    true
+  #if speed > 60
+  #  true
+  #elsif speed >40 && speed <60
+  #  false
+  #else 
+  #  true
 end
 
 def not_safe?(speed)
-  speed >40 && speed <60 ? true :false	
+  #speed >40 && speed <60 ? true :false	
 end
